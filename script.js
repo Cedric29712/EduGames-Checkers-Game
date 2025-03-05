3926aba4a55a658b02b8a5ae4e9585e5ac94dff1
Cedric29712 <dalancedric@gmail.com>
2025-03-05 15:00:50 +0800

    Update script.js

--- script.js
+++ script.js
@@ -338,7 +338,43 @@ function restartGame() {
     createBoard();
     updatePieceCount();
 }
-  
+
+ const fetchingData = async () => {
+    const url = `https://opentdb.com/api.php?amount=30&category=9&type=multiple&encode=url3986`;
+
+    const response = await fetch(url);
+    if (response) {
+        const data = await response.json();
+        questions = data.results.map((importedQuestion) => {
+            const formattedQuestion = {
+                question: importedQuestion.question,
+                possible_answers: [
+                    importedQuestion.correct_answer,
+                    ...importedQuestion.incorrect_answers,
+                ],
+                correct_answer: importedQuestion.correct_answer,
+                incorrect_answers: [...importedQuestion.incorrect_answers],
+            };
+            const answerChoices = [...formattedQuestion.incorrect_answers];
+            randomNumber = Math.floor(Math.random() * 3) + 1;
+            answerChoices.splice(
+                randomNumber - 1,
+                0,
+                formattedQuestion.correct_answer
+            );
+
+            answerChoices.forEach((choice, index) => {
+                formattedQuestion["choice" + (index + 1)] = choice;
+            });
+            return formattedQuestion;
+        });
+        startGame();
+    }
+};
+const start = document.querySelector("#start");
+
+start.addEventListener("click", fetchingData);
+    
  const questions = [
     {
         question: "What is the derivative of sin(x)?",
